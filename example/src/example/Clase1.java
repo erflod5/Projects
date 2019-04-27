357b2f81da124e9e44f64263cd26212270fc9c23
unknown <2883388711710@ingenieria.usac.edu.gt>
2019-04-27 09:38:11 -0600

    first commit

---
+++ example/src/example/Clase1.java
@@ -0,0 +1,17 @@
+package example;
+
+public class Clase1 {
+
+	public static void main(String[] args) {
+		Clase1 c1 = new Clase1();
+		c1.iniciar();
+	}
+	
+	public void iniciar() {
+		
+		System.out.println(x);
+	}
+	
+	int x = 3;
+
+}
